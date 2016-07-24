e5224c15f9471bca43d7a54b6e364c351ef40404
sandrafp <sandrasdfp@gmail.com>
2016-07-24 15:49:34 -0400

    merge y adicion rama galeria mas etilos

---
+++ bootstrap-3.3.6-dist/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    $('.capa-1').mouseenter(function() {
+		$('#capa-1').removeClass('hidden');
+        $('#capa-1').show();
+    }).mouseleave(function() {      
+            $('#capa-1').hide();
+    });
+    $('.capa-2').mouseenter(function() {
+		$('#capa-2').removeClass('hidden');
+        $('#capa-2').show();
+    }).mouseleave(function() {      
+            $('#capa-2').hide();
+    });
+  	$('.capa-3').mouseenter(function() {
+		$('#capa-3').removeClass('hidden');
+        $('#capa-3').show();
+    }).mouseleave(function() {      
+            $('#capa-3').hide();
+    });
+    	$('.capa-4').mouseenter(function() {
+		$('#capa-4').removeClass('hidden');
+        $('#capa-4').show();
+    }).mouseleave(function() {      
+            $('#capa-4').hide();
+    });
+    	$('.capa-5').mouseenter(function() {
+		$('#capa-5').removeClass('hidden');
+        $('#capa-5').show();
+    }).mouseleave(function() {      
+            $('#capa-5').hide();
+    });
+    	$('.capa-6').mouseenter(function() {
+		$('#capa-6').removeClass('hidden');
+        $('#capa-6').show();
+    }).mouseleave(function() {      
+            $('#capa-6').hide();
+    });
+    	$('.capa-7').mouseenter(function() {
+		$('#capa-7').removeClass('hidden');
+        $('#capa-7').show();
+    }).mouseleave(function() {      
+            $('#capa-7').hide();
+    });
+    	$('.capa').mouseenter(function() {
+		$('#capa').removeClass('hidden');
+        $('#capa').show();
+    }).mouseleave(function() {      
+            $('#capa').hide();
+    });
+});
